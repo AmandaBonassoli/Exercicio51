338a2b4830e337fb5fbb7e065c37a179c82fd823
AmandaBonassoli <bonassoli37@gmail.com>
2016-05-02 23:58:47 -0300

    Added files via upload

--- Exercicio51.java
+++ Exercicio51.java
@@ -1,9 +1,28 @@
-import java.util.Scanner;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Exercicio51 {
-	public static void main (String [] args) {
+	
+		static JFrame tela;
+		static JButton botaoAdicionar = new JButton(new ImageIcon("imagem\\adicionar.png"));
 		
-		Scanner teclado = new Scanner(System.in);
+		public static void main(String[] args) {
+		tela = new JFrame();
+		Tela();
+		}
 		
+		static void Tela() {
+			
+			tela.setTitle("Menu");
+			tela.setSize(1600,900);
+			tela.setLocation(0, 0);
+			tela.getContentPane().setBackground(Color.white);
+			tela.setResizable(false);
+			tela.setVisible(true);
+			tela.add(botaoAdicionar).setBounds(20, 20, 0, 0); 
+			
+
 	}
 }
